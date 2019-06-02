eace260f8e53c5894d05da78f3be8488a772b814
Minal Kharche <minal.berkeley@gmail.com>
2019-06-01 19:35:05 -0700

    added sample questions and answers in javascript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        "question": "What is CSS stand for?",
+        "answer1": "Cascading Style Sheets",
+        "answer2": "css",
+        "answer3": "css",  
+        "answer4": "css"
+    },
+    {
+        "question": "What is HTML stand for?",
+        "answer1": "Cascading Style Sheets",
+        "answer2": "Hypertext Markup language",
+        "answer3": "css",  
+        "answer4": "css"
+    }
+
+];
+
+console.log(questions);
+var gifs = ["assets/images/gif1.gif", "assets/images/gif2.gif","assets/images/gif3.gif"];
+var correctAns = ["Cascading Style Sheets","Hypertext Markup language"];
+
+var count = 0;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var allowedTime = 10;
+var logic;
+
