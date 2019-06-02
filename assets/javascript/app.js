d2afa5b267428b02727d1bf3d12709c6573e61e0
Minal Kharche <minal.berkeley@gmail.com>
2019-06-01 20:34:00 -0700

    added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-var questions = [
+//questions and answers Object
+var questionsData = [
     {
         "question": "What is CSS stand for?",
         "answer1": "Cascading Style Sheets",
@@ -17,13 +18,32 @@ var questions = [
 ];
 
 console.log(questions);
+//correct answers and gif images array
 var gifs = ["assets/images/gif1.gif", "assets/images/gif2.gif","assets/images/gif3.gif"];
 var correctAns = ["Cascading Style Sheets","Hypertext Markup language"];
 
+//global variable
 var count = 0;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var allowedTime = 10;
-var logic;
+var allowedTime = 11;
+var moveGame;
 
+$(".answers").hide();
+
+//onclick start button
+$("#start").on("click", function () {
+    $("#start").attr("style", "display:none");
+    moveGame = setInterval(majorMoveGame, 1000);
+});
+//functions
+majorMoveGame = function(){
+    allowedTime--;
+    $("#timer").text("Timer: " + allowedTime);
+
+    if (allowedTime === 0 && count < questionsData.length) {
+        clearInterval(moveGame);
+        
+    }
+}
